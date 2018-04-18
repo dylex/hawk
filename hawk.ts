ccf1d289abb9b3349a0472e8b68eadfb22905939
Dylan Simon <dylan@dylex.net>
2018-04-17 22:05:27 -0400

    Add some js stuff, not much linked in

---
+++ hawk.ts
@@ -0,0 +1,86 @@
+namespace __HaWK__ {
+
+  type Block = {
+    def: boolean,
+    exc?: RegExp
+  };
+
+  interface Blocks {
+    def: Block;
+    [type:string]: Block;
+  }
+
+  const blocks: Blocks = {
+    def: {
+      def: false
+    },
+    LINK: {
+      def: true
+    }
+  };
+
+  type LoadedElement = HTMLElement&{src?:string};
+
+  function blockTest(type: string, src: string|undefined) {
+    let block = blocks[type/*.toUpperCase()*/];
+    if (!block) {
+      type += "<unknown>";
+      block = blocks.def;
+    }
+    let res = block.def;
+    if (src && block.exc && block.exc.test(src))
+      res = !res;
+    if (!res || res !== block.def)
+      console.log((res ? "allowing" : "blocking") + " " + type + " " + src);
+    return res;
+  }
+
+  function initDocument(doc: HTMLDocument) {
+    doc.addEventListener('focusin', function focusin(event) {
+      if (event.target instanceof HTMLInputElement) {
+        console.log('focusin ' + event.target.type);
+      }
+    }, false);
+    doc.addEventListener('beforeload', function beforeload(event) {
+      const el = <LoadedElement>event.target;
+      if (!(el instanceof HTMLElement))
+        return;
+      if (!blockTest(el.tagName, el.src)) {
+        event.preventDefault();
+        if (el.parentNode)
+          el.parentNode.removeChild(el);
+      } else if (el instanceof HTMLIFrameElement) {
+        if (el.contentDocument)
+          /* this does not work, possibly because the document hasn't started loading yet */
+          initDocument(el.contentDocument);
+      }
+    }, true);
+  }
+
+  initDocument(document);
+
+  const links = document.getElementsByTagName("a");
+
+  var linkSelected = -1;
+
+  function linkFocus(n: number) {
+    linkSelected = n;
+    links[n].focus();
+  }
+
+  export function linkSelect(link: string, match: string) {
+    const el = document.querySelector('link[rel=' + link + ']');
+    if (el instanceof HTMLLinkElement) {
+      location.assign(el.href);
+      return;
+    }
+    const regex = RegExp(match);
+    for (var i = linkSelected+1; i < links.length; i++) {
+      if (regex.test(links[i].text)) {
+        linkFocus(i);
+        break;
+      }
+    }
+    linkSelected = -1;
+  }
+}
